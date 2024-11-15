99afd2ae15674f3692e7bf2be3caf93aeac965a1
ezedin ebrahim <ezedinebrahim131@gmail.com>
2024-11-15 21:57:37 +0300

    to commit1

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,24 +9,21 @@ exports.authenticate = async (req, res, next) => {
   if (authHeader && authHeader.startsWith("Bearer ")) {
     token = authHeader.split(" ")[1]; // Extract the token from the header
 
-    // Verify the token
     jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
       if (err) {
         console.error("Token verification error:", err.message); // Log the error
         return res.status(401).json({ message: "Invalid token" });
       }
 
-      // Fetch the user from the database using the ID from the decoded token
-      const user = await User.findById(decoded.user.id); // Ensure this matches your token structure
+      const user = await User.findById(decoded.user.id);
       if (!user) {
         return res
           .status(401)
           .json({ message: "User not found, unauthorized" });
       }
 
-      // Attach the user object to the request for further use in the application
       req.user = user;
-      next(); // Proceed to the next middleware or route handler
+      next();
     });
   } else {
     return res.status(401).json({
